345bd5f617d91ddd38de86dad8b842d487bd7b91
Replit user <>
2023-10-29 06:57:46 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
-# MongoDB and Mongoose Challenges
+# MongoDB and Mongoose Challenges - FreeCodeCamp Solution
 
-This is the boilerplate for the MongoDB and Mongoose lessons. Instructions for completing these lessons start at https://www.freecodecamp.org/learn/apis-and-microservices/mongodb-and-mongoose/
+This repository contains my solution to the MongoDB and Mongoose challenges as part of the FreeCodeCamp curriculum. It's designed to help you understand the intricacies of database operations using MongoDB and the Mongoose library.
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js and npm installed on your machine.
+- A MongoDB Atlas account or local MongoDB setup.
+
+### Setup
+
+After cloning this repository, install the required dependencies:
+
+```bash
+npm install
+```
+
+Create a `.env` file in the root of the project. Populate it with your MongoDB connection details:
+
+```bash
+MONGO_USERNAME=your_username
+MONGO_PASSWORD=your_password
+CLUSTER=your_cluster_name
+```
+
+**Note**: Make sure to replace `your_username`, `your_password`, and `your_cluster_name` with your actual MongoDB Atlas credentials.
+
+Start the server:
+
+```bash
+npm start
+```
+
+## Endpoints
+
+Here are some of the primary endpoints you can explore:
+
+- **GET** `/`: Main endpoint that serves the index page.
+- **GET** `/_api/create-and-save-person`: Creates and saves a new person record in the database.
+- **POST** `/_api/create-many-people`: Creates and saves multiple person records in the database.
+- **POST** `/_api/find-all-by-name`: Finds all people by a specific name.
+- **POST** `/_api/find-one-by-food`: Finds a person by their favorite food.
+- **GET** `/_api/find-by-id`: Finds a person by their unique ID.
+- **POST** `/_api/find-edit-save`: Finds a person by ID, edits their data, then saves the updated record.
+- **POST** `/_api/find-one-update`: Finds a person by name and updates their age.
+- **POST** `/_api/remove-one-person`: Removes one person by their ID.
+- **POST** `/_api/remove-many-people`: Removes all people who match a given name.
+- **POST** `/_api/query-tools`: Demonstrates a chained search query tool.
+
+Each endpoint serves a specific function in the database, whether it's creating, reading, updating, or deleting records.
--- myApp.js
+++ myApp.js
@@ -1,70 +1,190 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const connectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.CLUSTER}.ecttduz.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 
-let Person;
-
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+const createAndSavePerson = (done, name = "Adrian", age = 35, favoriteFoods = ["Pierogis", "Paczkis"]) => {
+  let newPerson = new Person({ name, age, favoriteFoods });
+  newPerson.save((err, data) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(data);
+    done(null, data);
+  });
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+var peopleArray = [
+  {
+    name: "Ludwig van Beethoven",
+    age: 56,
+    favoriteFoods: ["Bread", "Fish", "Wine"]
+  },
+  {
+    name: "Friedrich Nietzsche",
+    age: 55,
+    favoriteFoods: ["Pasta", "Bread", "Fruits"]
+  },
+  {
+    name: "Bob Dylan",
+    age: 80,
+    favoriteFoods: ["Coffee", "Burgers", "Pie"]
+  },
+  {
+    name: "Plato",
+    age: 80,
+    favoriteFoods: ["Olives", "Wine", "Fig"]
+  },
+  {
+    name: "Jimi Hendrix",
+    age: 27,
+    favoriteFoods: ["Steak", "Eggs", "Juice"]
+  },
+  {
+    name: "Immanuel Kant",
+    age: 79,
+    favoriteFoods: ["Bread", "Cheese", "Wine"]
+  },
+  {
+    name: "John Lennon",
+    age: 40,
+    favoriteFoods: ["Tea", "Toast", "Eggs"]
+  }
+];
+
+
+const createManyPeople = (arrayOfPeople = peopleArray, done) => {
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(data);
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, function (err, personFound) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(personFound);
+    done(null, personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function (err, foodFound) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(foodFound);
+    done(null, foodFound);
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function (err, personFound) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(personFound);
+    done(null, personFound);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, function (err, personFound) {
+    if (err) {
+      return console.log(err);
+    }
+    personFound.favoriteFoods.push(foodToAdd);
+    personFound.save((err, data) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(data);
+      done(null, data);
+    });
+  });
 };
+
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, function (err, personFound) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(personFound);
+    done(null, personFound);
+  });
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, function (err, personFound) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(personFound);
+    done(null, personFound);
+  });
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+  .sort({name: 1})
+  .limit(2)
+  .select({age: 0})
+  .exec(function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    done(null, data);
+    
+  });
 };
 
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
+  /** **Well Done !!**
+  /* You completed these challenges, let's go celebrate !
+   */
 
-//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+  //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
-exports.PersonModel = Person;
-exports.createAndSavePerson = createAndSavePerson;
-exports.findPeopleByName = findPeopleByName;
-exports.findOneByFood = findOneByFood;
-exports.findPersonById = findPersonById;
-exports.findEditThenSave = findEditThenSave;
-exports.findAndUpdate = findAndUpdate;
-exports.createManyPeople = createManyPeople;
-exports.removeById = removeById;
-exports.removeManyPeople = removeManyPeople;
-exports.queryChain = queryChain;
+  exports.PersonModel = Person;
+  exports.createAndSavePerson = createAndSavePerson;
+  exports.findPeopleByName = findPeopleByName;
+  exports.findOneByFood = findOneByFood;
+  exports.findPersonById = findPersonById;
+  exports.findEditThenSave = findEditThenSave;
+  exports.findAndUpdate = findAndUpdate;
+  exports.createManyPeople = createManyPeople;
+  exports.removeById = removeById;
+  exports.removeManyPeople = removeManyPeople;
+  exports.queryChain = queryChain;
